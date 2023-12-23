a2f835d75b68470e2a31e95e1d93b94cd19bfb7c
Hosung99 <shson1217@kw.ac.kr>
2023-12-23 01:20:24 +0000

    [Update] cpp01 ex04수정

--- cpp01/ex04/replace.cpp
+++ cpp01/ex04/replace.cpp
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 12:32:45 by codespace         #+#    #+#             */
-/*   Updated: 2023/12/20 12:33:46 by codespace        ###   ########.fr       */
+/*   Updated: 2023/12/23 01:19:30 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,20 @@
 std::string	replace(std::fstream &file, std::string *char_to_string)
 {
 	size_t pos;
-	std::string	str;
-	std::string	str_tmp;
+	std::string	str,line;
+
+	while (std::getline(file, line))
+		str += line + "\n";
 	while (1)
 	{
-		char *tmp = new char[char_to_string[1].length() + 1];
-		file.read(tmp, char_to_string[1].length());
-		str_tmp = std::string(tmp);
-		pos = str_tmp.find(char_to_string[1]);
+		pos = str.find(char_to_string[1]);
 		if (pos != std::string::npos)
 		{
-			str_tmp.erase(pos, char_to_string[1].length());
-			str_tmp.insert(pos, char_to_string[2]);
+			str.erase(pos, char_to_string[1].length());
+			str.insert(pos, char_to_string[2]);
 			pos += char_to_string[1].length();
 		}
-		str += str_tmp;
-		delete [] tmp;
-		if (file.eof())
+		else
 			break ;
 	}
 	return (str);
